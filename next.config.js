2ce0e8189c4f3235add02f0c89c6aa6aa47930cd
Ethan-dev-mc <ethan.studio@gmail.com>
2026-04-04 22:59:36 -0600

    fix: allow Unsplash images in Next.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,15 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'plus.unsplash.com' },
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
     ],
   },
 }
